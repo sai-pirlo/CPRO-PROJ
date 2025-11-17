2089f5204e3d9171f158ad9ee80dd0e965b3e774
sai-pirlo <saicharanreddygoli92@gmail.com>
2025-11-17 23:24:07 +0530

    Update game.h

--- game.h
+++ game.h
@@ -1,8 +1,5 @@
 #define game_h
-
 #include <stdio.h> 
-
-
 #define boardsize 20
 #define maxlinks 1000
 enum dot {
@@ -10,48 +7,29 @@ enum dot {
     dot_red,     
     dot_black    
 };
-
 enum player {
     player_red,
     player_black
 };
-
 struct dots {
     int row;
     int column;
 };
-
 struct link {
     struct dots p1; 
     struct dots p2; 
 };
-
 struct gamestate {
     enum dot board[boardsize][boardsize];
-    
-   
     struct link redlinks[maxlinks];
     int numredlinks;
-    
     struct link blacklinks[maxlinks];
     int numblacklinks;
-    
-
     enum player currentplayer;
 } ;
 
+void initializegame(struct gamestate *game);
 
-void initializegame(struct gamestate *game){
+void printboard(const struct gamestate *game);
 
-};
-
-
-void printboard(const struct gamestate *game){
-
-};
-
-
-void altturn(struct gamestate *game){
-
-};
-
+void altturn(struct gamestate *game);
